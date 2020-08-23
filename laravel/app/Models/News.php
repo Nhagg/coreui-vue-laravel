f9610a429d03e62c8c2cdcef4ca2f919b1eaef36
dvn <duoc.fithou@gmail.com>
2020-08-23 23:37:20 +0700

    view

---
+++ laravel/app/Models/News.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent as Model;
+
+/**
+ * Class News
+ * @package App\Models
+ * @version August 23, 2020, 9:57 am UTC
+ *
+ * @property string $short_name
+ * @property string $name
+ * @property string $content
+ */
+class News extends Model
+{
+
+    public $table = 'news';
+    
+
+
+
+    public $fillable = [
+        'short_name',
+        'name',
+        'content'
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'short_name' => 'string',
+        'name' => 'string'
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        
+    ];
+
+    
+}
---
+++ laravel/app/Repositories/NewsRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\News;
+use App\Repositories\BaseRepository;
+
+/**
+ * Class NewsRepository
+ * @package App\Repositories
+ * @version August 23, 2020, 9:57 am UTC
+*/
+
+class NewsRepository extends BaseRepository
+{
+    /**
+     * @var array
+     */
+    protected $fieldSearchable = [
+        'short_name',
+        'name',
+        'content'
+    ];
+
+    /**
+     * Return searchable fields
+     *
+     * @return array
+     */
+    public function getFieldsSearchable()
+    {
+        return $this->fieldSearchable;
+    }
+
+    /**
+     * Configure the Model
+     **/
+    public function model()
+    {
+        return News::class;
+    }
+}
